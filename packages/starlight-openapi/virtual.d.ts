0156a69c2d36f1a4ca6aa6c42dbe9227473ebb04
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-02-17 23:33:59 +0100

    chore: update @hideoo/eslint-config to version 3

--- packages/starlight-openapi/virtual.d.ts
+++ packages/starlight-openapi/virtual.d.ts
@@ -1,7 +1,5 @@
 declare module 'virtual:starlight-openapi-schemas' {
-  const StarlightOpenAPISchemas: {
-    [base: string]: import('./libs/schema').Schema
-  }
+  const StarlightOpenAPISchemas: Record<string, import('./libs/schema').Schema>
 
   export default StarlightOpenAPISchemas
 }
