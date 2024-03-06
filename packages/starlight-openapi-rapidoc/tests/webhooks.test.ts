05068a0b5ca582552eb1bde05adebb58818c2d7a
Jefferson Rafael Kozerski <jeff.drumgod@gmail.com>
2024-03-06 15:19:13 -0300

    Remove unnecessary test case for displaying operation

--- packages/starlight-openapi-rapidoc/tests/webhooks.test.ts
+++ packages/starlight-openapi-rapidoc/tests/webhooks.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from './test'
+import { test } from './test'
 
 test('uses the operation summary for title', async ({ docPage }) => {
   await docPage.goto('/v3/animals/webhooks/newanimal/')
@@ -10,12 +10,4 @@ test('falls back to the operation ID for title', async ({ docPage }) => {
   await docPage.goto('/v3/animals/webhooks/newcat/')
 
   await docPage.expectToHaveTitle('newCat')
-})
-
-test('displays the operation', async ({ docPage }) => {
-  await docPage.goto('/v3/animals/webhooks/newanimal/')
-
-  await expect(docPage.getByText('POST')).toBeVisible()
-  expect(await docPage.getContent().getByRole('group').count()).toBe(1)
-  await expect(docPage.getByText('New animal details')).toBeVisible()
 })
