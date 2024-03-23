cc93a4d67877f7c752757ae8a7dd68183c4c6439
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-03-23 11:47:46 +0100

    feat: displays the description of objects before their properties

--- packages/starlight-openapi/playwright.config.ts
+++ packages/starlight-openapi/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], headless: true },
     },
   ],
   use: {
